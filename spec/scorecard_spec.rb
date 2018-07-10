75be490f0b4a083f95ddb8267450b0dd311fee22
mattb20 <matt_burgoyne@live.co.uk>
2018-07-10 13:22:54 +0100

    roll method needs adding to scorecard class, test failing

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -2,6 +2,10 @@ require 'scorecard'
 describe Scorecard do
   it ('should have a method to return the score') do
     scorecard = Scorecard.new
-    expect(scorecard.score).to eq 0
+    expect(scorecard.score).to eq 0;
+  end
+  it ('will track the score if I score 4') do
+    scorecard = Scorecard.new
+    expect(scorecard.roll(4)).to eq 4;
   end
 end

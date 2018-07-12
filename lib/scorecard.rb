6498d70aa99cb7d9dbd587c4fa38d543fb294637
mattb20 <matt_burgoyne@live.co.uk>
2018-07-12 12:20:03 +0100

    test written in scorecard spec to store rolls inside array acting as the frame, failing needs passing by adding to handlescore method

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,27 +1,20 @@
 require 'frame'
 class Scorecard
   attr_accessor :score
-  attr_accessor :frame
+  attr_accessor :frames
   attr_accessor :current_frame
-  def initialize(score = 0, bonus=false, frame = Frame.new)
+  def initialize(score = 0, bonus=false, frameset = Frame.new, current_frame = Frame.new)
     @gamescore = score
     @bonus = bonus
-    @frame = frame
-    @current_frame = [];
+    @frameset = frameset
+    @current_frame = current_frame;
   end
   def roll(pins)
     handlescore(pins)
   end
   private
   def handlescore(pins)
-    self.current_frame.push(pins)
-    if self.current_frame.complete?
-      return frame.reduce(:*)
-    end
-
-    case pins
-    when 10
-      @bonus = true
-    end
   end
+
+
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,24 +1,10 @@
 require 'scorecard'
 describe Scorecard do
   let(:frame){double('frame')};
-  it ('should have a method to return the score') do
+  it 'should store scores in a subarray for each frame inside a larger array' do
     scorecard = Scorecard.new
-    expect(scorecard.score).to eq 0;
-  end
-  it ('will track the score if I score 4') do
-    scorecard = Scorecard.new
-    expect(scorecard.roll(4)).to eq 4;
-  end
-  it ('will track the score if I score 4 and then 5') do
-    scorecard = Scorecard.new
-    scorecard.roll(4);
-    expect(scorecard.roll(5)).to eq 9;
-  end
-  it ('should double the next two rolls following a strike') do
-    scorecard = Scorecard.new;
-    scorecard.roll(10);
     scorecard.roll(1);
-    expect(scorecard.roll(2)).to eq 16;
+    expect(scorecard.roll(2)).to eq [1,2];
 
   end
 end
